ebf862440f122dfc717cb8fcf94acb946cc6c862
Juan Ignacio Falibene <juanignaciofalibene@gmail.com>
2024-02-28 20:23:56 +0100

    Componente credits creado

---
+++ src/Credits.jsx
@@ -0,0 +1,21 @@
+const Credits = () => {
+  return (
+    <div className='credits'>
+      <p>
+        <a
+          href='https://github.com/juanfalibene/02-memory-cards'
+          target='_blank'
+          rel='noreferrer'
+        >
+          view on github
+        </a>
+        <br />
+        <a href='https://juanfalibene.com' target='_blank' rel='noreferrer'>
+          juanfalibene.com
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Credits;
